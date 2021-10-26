4b73e4f1458ee235fe2843988c49fd4b448be385
guimmamanna <59138078+guimmamanna@users.noreply.github.com>
2021-10-26 18:36:39 +0100

    trying to set code to names

--- csv files/clean.php
+++ csv files/clean.php
@@ -1,12 +1,19 @@
 <?php
+//read csv file
+$file = 'harvest data-clean.csv';
+$error_log_file = fopen(".error_log_file.txt", "w");
+//crop codes transform to name
+$var = new county('w', 'Wheat');
+$var2 = new county('B', 'Barley');
+$var3 = new county('M', 'Maize');
+$var4 = new county('BE', 'Beetroot');
+$var5 = new county('C', 'Carrot');
+$var6 = new county('PO', 'Potatoes');
+$var7 = new county('PA', 'Parsnips');
+$var8 = new county('O', 'Oats');
+// array of crop codes name
+$array_of_county= array($var, $var2,$var3,$var4,$var5,$var6,$var7,$var8,);
+foreach ($file as $K)
+//obtain an array called 
 
-// open harvest data csv file
-$path = fopen('harvest data - clean.csv', 'r');
-
-// read file
-while (($data = fgetcsv($path, 1000, ',')) !==FALSE)
-{
-    echo"<pre>"; print_r($data);
-}
-fclose($path);
 ?>
